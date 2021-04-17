58bffdac4f0e789cb052d534c632b6e528f4d093
iemong <kiyoshidainagon@gmail.com>
2021-04-18 00:06:18 +0900

    eslintにいい感じの設定を追加する

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.export = {
-    plugins: ["prettier"],
-    extends: ["plugin:prettier/recommended", "prettier"],
+    root: true,
+    parser: '@typescript-eslint/parser',
+
+    plugins: [
+        '@typescript-eslint',
+        'react-hooks',
+        'jsx-a11y',
+        'import',
+        'prettier',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'prettier',
+    ],
 }
