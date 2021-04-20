c461b6a7e1d38167a1acf8ae200a5e9bb294fa9a
iemong <kiyoshidainagon@gmail.com>
2021-04-21 01:06:10 +0900

    追加

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,9 @@
-const tailwindcss = require('tailwindcss')
-const postcssPresetEnv = require('postcss-preset-env')
-const autoprefixer = require('autoprefixer')
-
 module.exports = {
     plugins: [
-        tailwindcss(),
-        autoprefixer(),
-        postcssPresetEnv({ browsers: 'last 2 versions', stage: 1 }),
+        require('tailwindcss'),
+        require('postcss-preset-env')({
+            browsers: 'last 2 versions',
+            stage: 1,
+        }),
     ],
 }
-
-module.exports = (opts = {}) => {
-    checkOpts(opts)
-    return {
-        postcssPlugin: 'postcss-dark-theme-class',
-        Once(root, { result }) {
-            root.walkAtRules((atrule) => {})
-        },
-    }
-}
-
-module.exports.postcss = true
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,12 @@ module.exports = {
         enabled: process.env.NODE_ENV === 'production',
         content: ['./index.html', './src/**/*.tsx', './src/**/*.ts'],
     },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
